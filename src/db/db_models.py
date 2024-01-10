030a843d24bb881091f013ed353f10b2c7c4f7ac
Abdallah Serghine <abdallah.serghine@olx.pl>
2024-01-10 22:13:34 +0100

    Setup many-to-many relation experiments<->teams

--- src/db/db_models.py
+++ src/db/db_models.py
@@ -1,10 +1,15 @@
 import datetime
 
 from sqlalchemy import TIMESTAMP
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import Table
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
@@ -18,6 +23,24 @@ engine = None
 session: Session = None
 
 
+experiments_teams = Table(
+    'experiments_teams',
+    Base.metadata,
+    Column(
+        'experiment_id',
+        Integer,
+        ForeignKey('experiment.id'),
+        primary_key=True,
+    ),
+    Column(
+        'team_id',
+        Integer,
+        ForeignKey('team.id'),
+        primary_key=True,
+    ),
+)
+
+
 class Experiment(Base):
     """Database model for experiments model."""
 
@@ -29,6 +52,10 @@ class Experiment(Base):
 
     sample_ratio: Mapped[int]
 
+    experiments: Mapped[list["Team"]] = relationship(
+        secondary=experiments_teams, uselist=True, lazy='selectin'
+    )
+
 
 class Team(Base):
     """Database model for team model."""
@@ -38,3 +65,7 @@ class Team(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
     name: Mapped[str]
+
+    experiments: Mapped[list["Experiment"]] = relationship(
+        secondary=experiments_teams, uselist=True, lazy='selectin'
+    )

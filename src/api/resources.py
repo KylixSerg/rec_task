0ee77f3a45263d16614db4e3897d025c82c76503
Abdallah Serghine <abdallah.serghine@olx.pl>
2024-01-11 23:49:52 +0100

    Add experiment listing endpoint

--- src/api/resources.py
+++ src/api/resources.py
@@ -1,15 +1,25 @@
 from flask import Blueprint
 from flask import jsonify
 from flask import request
+from marshmallow import EXCLUDE
 from marshmallow import Schema
 from marshmallow import ValidationError
 from marshmallow import fields as maf
+from marshmallow import post_load
+from marshmallow import pre_load
 from marshmallow import validate as mav
+from sqlalchemy import and_
+from sqlalchemy import asc
+from sqlalchemy import desc
 from sqlalchemy import exists
+from sqlalchemy import func
 from sqlalchemy import select
+from sqlalchemy import text
+from sqlalchemy.orm import selectinload
 
 from db.db_models import Experiment
 from db.db_models import Team
+from db.db_models import experiments_teams
 from db.db_models import get_session
 
 bp = Blueprint('rec_task_resources', __name__, url_prefix='')
@@ -29,14 +39,83 @@ class ExperimentSchema(Schema):
     team_ids = maf.List(maf.Integer(), load_only=True, required=True, validate=mav.Length(1, 2))
 
 
+class BaseQueryArgsSchema(Schema):
+    page = maf.Integer(load_default=0)
+    limit = maf.Integer(load_default=25)
+    order_by = maf.String(load_default='asc', validate=mav.OneOf(('asc', 'desc')))
+    sort_by = maf.String()
+
+    @post_load
+    def normalize_qas(self, data, **kwargs):
+        data['page'] = 0 if data['page'] < 0 else data['page']
+        data['limit'] = 25 if data['limit'] < 1 else data['limit']
+        if data['page'] > 0:
+            data['page'] -= 1
+        return data
+
+
+class ExperimentListQAS(BaseQueryArgsSchema):
+    sort_by = maf.String(validate=mav.OneOf(('id', 'sample_ratio')), load_default='id')
+
+    class Meta:
+        unknown = EXCLUDE
+
+
 @bp.route("/experiments")
 def get_experiments():
-    return '', 200
+    """List experiments."""
+
+    session = get_session()
+
+    try:
+        filters = ExperimentListQAS().load(request.args)
+        # For a quick and dirty way to deserialize lists
+        if team_ids := request.args.getlist("team_ids[]"):
+            try:
+                team_ids = [int(tid) for tid in team_ids]
+                filters['team_ids[]'] = team_ids
+            except ValueError as ex:
+                return ValidationError(str(ex))
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    order_by_func = asc if filters['order_by'] == 'asc' else desc
+
+    query = (
+        select(Experiment)
+        .offset(filters['page'] * filters['limit'])
+        .limit(filters['limit'])
+        .order_by(order_by_func(text(f"{filters['sort_by']}")))
+        .options(selectinload(Experiment.teams))
+    )
+
+    if team_ids:
+        # TODO: extract to a MAT VIEW
+        filter_query = (
+            select(
+                experiments_teams.c.experiment_id,
+                func.array_agg(experiments_teams.c.team_id).label("team_ids"),
+            )
+            .group_by(experiments_teams.c.experiment_id)
+            .cte()
+        )
+
+        query = query.join(
+            filter_query,
+            and_(
+                filter_query.c.team_ids.op("&&")(team_ids),
+                filter_query.c.experiment_id == Experiment.id,
+            ),
+        )
+
+    data = ExperimentSchema(many=True).dump(session.execute(query).scalars())
+    meta = {**filters, "items": len(data), "page": filters['page'] + 1}
+
+    return {"data": data, "meta": meta}
 
 
 @bp.route("/experiments", methods=["POST"])
 def create_experiment():
-
     try:
         item = ExperimentSchema().load(request.json)
     except ValidationError as err:
@@ -61,7 +140,6 @@ def create_experiment():
 
 @bp.route("/teams", methods=["POST"])
 def create_team():
-
     try:
         item = TeamSchema().load(request.json)
     except ValidationError as err:
--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,5 +1,6 @@
 from unittest.mock import ANY
 
+from tests.factories import ExperimentFactory
 from tests.factories import TeamFactory
 
 
@@ -56,3 +57,67 @@ def test_create_experiment(client, faker):
         'sample_ratio': payload['sample_ratio'],
         'teams': [{'id': team.id, 'name': team.name}],
     }
+
+
+def test_get_experiments(client):
+    """Test experiment listing."""
+
+    e1, e2, e3, e4 = ExperimentFactory.create_batch(4)
+    t1, t2, t3 = TeamFactory.create_batch(3)
+
+    e1.teams = [t1, t2]
+    e2.teams = [t2, t3]
+
+    # no qs are required
+    ret = client.get('/experiments', query_string={})
+    assert ret.status_code == 200
+    # defaulted to order by id
+    assert [exp["id"] for exp in ret.json["data"]] == [e1.id, e2.id, e3.id, e4.id]
+    assert ret.json['meta'] == {
+        'items': 4,
+        'limit': 25,
+        'order_by': 'asc',
+        'page': 1,
+        'sort_by': 'id',
+    }
+
+    # custom pagination/sorting
+    qs = {"page": 2, "limit": 2, "order_by": "desc"}
+
+    ret = client.get('/experiments', query_string=qs)
+    assert ret.status_code == 200
+    # descending order / 2nd page for a 2 items per page partition
+    assert [exp["id"] for exp in ret.json["data"]] == [e2.id, e1.id]
+    assert ret.json['meta'] == {
+        'items': 2,
+        'limit': 2,
+        'order_by': 'desc',
+        'page': 2,
+        'sort_by': 'id',
+    }
+
+    # filter by team_ids
+    qs = {"team_ids[]": [t1.id, t2.id]}
+
+    ret = client.get('/experiments', query_string=qs)
+    assert ret.status_code == 200
+    assert ret.json["data"] == [
+        {
+            'description': e1.description,
+            'id': e1.id,
+            'sample_ratio': e1.sample_ratio,
+            'teams': [
+                {'id': t1.id, 'name': t1.name},
+                {'id': t2.id, 'name': t2.name},
+            ],
+        },
+        {
+            'description': e2.description,
+            'id': e2.id,
+            'sample_ratio': e2.sample_ratio,
+            'teams': [
+                {'id': t2.id, 'name': t2.name},
+                {'id': t3.id, 'name': t3.name},
+            ],
+        },
+    ]
